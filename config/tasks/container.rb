2eadde0b58587731d8ac3339d561a028337ec737
Makoto Ujyu <ainehanta@hotmail.co.jp>
2014-08-21 15:17:51 +0900

    [config] Add all container start task

--- config/tasks/container.rb
+++ config/tasks/container.rb
@@ -6,3 +6,14 @@ task 'status' => 'load_containers' do
     puts "Status of #{container_name}: #{c.state}"
   end
 end
+
+task 'start' => 'load_containers' do
+  container_names = @containers.keys
+
+  container_names.each do |container_name|
+    c = LXC::Container.new(container_name)
+    c.start
+    puts "Start:#{container_name}"
+  end
+end
+
