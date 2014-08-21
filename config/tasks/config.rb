b10554c92fb6c7457856f9caef95e7671a1a73d8
tech7nica <tech7nica@gmail.com>
2014-08-21 16:50:17 +0900

    [config] Nagios config fix

--- config/tasks/config.rb
+++ config/tasks/config.rb
@@ -113,12 +113,12 @@ def config_zabbix_agent(container)
 end
 
 def config_nagios(container)
-  open("#{container['container_path']}/rootfs/","r+") do |f|
+  open("#{container['container_path']}/rootfs/etc/nagios/nagios.cfg","r+") do |f|
     f.flock(File::LOCK_EX)
     body = f.read
     p body.class
-    if body.match(/^(?!.*#\s*)broker_module=[[:print]]*/)
-      body.sub!(/^(?!.*#\s*)broker_module=[[:print]]*/,"broker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg");
+    if body.match(/^(?!.*#)\s*broker_module\s*=.*/)
+      body.sub!(/^(?!.*#)\s*broker_module\s*=.*/,"broker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg");
     elsif
       body.concat("#Added a broker_module\n");
       body.concat("broker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg\n");
@@ -130,12 +130,12 @@ def config_nagios(container)
     f.close
   end
 
-  open("#{container['container_path']}/rootfs/","r+") do |f|
+  open("#{container['container_path']}/rootfs/etc/nagios/ndo2db.cfg","r+") do |f|
     f.flock(File::LOCK_EX)
     body = f.read
-    body.sub!(/db_name=[[:print:]]*/,"db_name=#{container['nagios']['database_name']}");
-    body.sub!(/(?<!ndo2)db_user=[[:print:]]*/,"db_user=#{container['nagios']['database_username']}");
-    body.sub!(/db_pass=[[:print:]]*/,"db_pass=#{container['nagios']['database_password']}");
+    body.sub!(/db_name\s*=.*/,"db_name=#{container['nagios']['database_name']}");
+    body.sub!(/(?<!ndo2)db_user\s*=.*/,"db_user=#{container['nagios']['database_username']}");
+    body.sub!(/db_pass\s*=.*/,"db_pass=#{container['nagios']['database_password']}");
     #print body
     f.rewind
     f.puts body
