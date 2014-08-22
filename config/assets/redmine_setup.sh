102c8dde9082d5989b90abce240ccb13303b4064
Makoto Ujyu <ainehanta@hotmail.co.jp>
2014-08-22 16:42:38 +0900

    [config] Unused Gem Remove(Rmagick)

--- config/assets/redmine_setup.sh
+++ config/assets/redmine_setup.sh
@@ -2,6 +2,6 @@
 
 service mysqld start
 cd /var/lib/redmine
-bundle install --without development test
+bundle install --without development test rmagick
 bundle exec rake generate_secret_token
 bundle exec rake --trace db:migrate RAILS_ENV=production
