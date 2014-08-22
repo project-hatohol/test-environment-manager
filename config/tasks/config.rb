6520dc008be234a10cb4b18cd8bf7780aa6f7ea1
tech7nica <tech7nica@gmail.com>
2014-08-22 10:12:30 +0900

    [config] Nagios settings fix

--- config/tasks/config.rb
+++ config/tasks/config.rb
@@ -174,13 +174,15 @@ def config_nagios(container)
     f.flock(File::LOCK_EX)
     body = f.read
     p body.class
-    if body.match(/^(?!.*#)\s*broker_module\s*=.*/)
-      body.sub!(/^(?!.*#)\s*broker_module\s*=.*/,"broker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg");
+    
+    if
+      body.match(/^(?!.*#)\s*broker_module\s*=.*/)
+      body.sub!(/^(?!.*#)\s*broker_module\s*=.*/,"\nbroker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg")
     elsif
-      body.concat("#Added a broker_module\n");
-      body.concat("broker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg\n");
+      body.concat("\n#Added by test-environment-manager\n");
+      body.concat("broker_module=/usr/lib64/nagios/brokers/ndomod.so config_file=/etc/nagios/ndomod.cfg\n")
     end
-    #print body
+    
     f.rewind
     f.puts body
     f.truncate(f.tell)
@@ -190,10 +192,31 @@ def config_nagios(container)
   open("#{container['container_path']}/rootfs/etc/nagios/ndo2db.cfg","r+") do |f|
     f.flock(File::LOCK_EX)
     body = f.read
-    body.sub!(/db_name\s*=.*/,"db_name=#{container['nagios']['database_name']}");
-    body.sub!(/(?<!ndo2)db_user\s*=.*/,"db_user=#{container['nagios']['database_username']}");
-    body.sub!(/db_pass\s*=.*/,"db_pass=#{container['nagios']['database_password']}");
-    #print body
+    
+    if
+      body.match(/^(?!.*#)\s*db_name\s*=.*/)
+      body.sub!(/^(?!.*#)\s*db_name\s*=.*/,"\ndb_name=#{container['nagios']['database_name']}")
+    elsif
+      body.concat("\n#Added by test-environment-manager\n");
+      body.concat("db_name=#{container['nagios']['database_name']}")
+    end
+    
+    if
+      body.match(/^(?!.*#)\s*db_user\s*=.*/)
+      body.sub!(/^(?!.*#)\s*db_user\s*=.*/,"\ndb_user=#{container['nagios']['database_username']}")
+    elsif
+      body.concat("\n#Added by test-environment-manager\n");
+      body.concat("db_user=#{container['nagios']['database_username']}")
+    end
+    
+    if
+      body.match(/^(?!.*#)\s*db_pass\s*=.*/)
+      body.sub!(/^(?!.*#)\s*db_pass\s*=.*/,"\ndb_pass=#{container['nagios']['database_password']}")
+    elsif
+      body.concat("\n#Added by test-environment-manager\n");
+      body.concat("db_user=#{container['nagios']['database_username']}")
+    end
+    
     f.rewind
     f.puts body
     f.truncate(f.tell)
