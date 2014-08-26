712b1f602e0c9fb308903f6163761e70fce9b2ed
Makoto Ujyu <ainehanta@hotmail.co.jp>
2014-08-26 11:31:02 +0900

    [config] Add namespace and container methods of each containers.

--- config/tasks/container.rb
+++ config/tasks/container.rb
@@ -1,3 +1,65 @@
+@containers.each do |container_name, container|
+  namespace container_name do
+
+    desc "#{container_name}:Show Container Status and IPs"
+    task "status" do
+      c = LXC::Container.new(container_name)
+      puts "Status of #{container_name}: #{c.state}"
+      puts "\tIP Address: #{c.ip_addresses}"
+    end
+
+    desc "Start Container:#{container_name}"
+    task "start" do
+      c = LXC::Container.new(container_name)
+      c.start
+      while true
+        sleep 1
+        break unless c.ip_addresses.empty?
+      end
+      puts "Start:#{container_name}(#{c.ip_addresses})"
+    end
+
+    desc "Shutdwon Container:#{container_name}"
+    task "shutdown" do
+      c = LXC::Container.new(container_name)
+      puts "Shutdown:#{container_name}(#{c.ip_addresses})"
+      c.shutdown
+    end
+
+    desc "Reboot Container:#{container_name}"
+    task "reboot" do
+      c = LXC::Container.new(container_name)
+      puts "Reboot:#{container_name}(#{c.ip_addresses})"
+      c.reboot
+    end
+
+    desc "Clone Container #{container_name} as base_container"
+    task 'build' do
+      if container.has_key?('base_container')
+        base_container = LXC::Container.new(container['base_container'])
+        base_container.shutdown
+
+        puts "Clone from #{container['base_container']} to #{container_name}"
+        base_container.clone(container_name)
+
+        puts "Done."
+      end
+    end
+
+    desc "Destroy Container:#{container_name}"
+    task "destroy" do
+
+      c = LXC::Container.new(container_name)
+      puts "Destroy:#{container_name}"
+      c.stop
+      c.destroy
+    end
+
+    desc "Call Destroy and Build tasks for #{container_name}"
+    task "rebuild" => ["destroy-#{container_name}", "build-#{container_name}"]
+  end
+end
+
 namespace 'container' do
 
   desc 'Show Container Status and IPs'
