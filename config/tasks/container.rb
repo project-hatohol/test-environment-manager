29e3b34abf1e72116431b65c0d9fb7535ce7b9d9
tech7nica <tech7nica@gmail.com>
2014-08-27 13:22:10 +0900

    [config] Mod build command Modify: add "build" command inside "shutdown" and "start" command.

--- config/tasks/container.rb
+++ config/tasks/container.rb
@@ -58,7 +58,7 @@
     task "rebuild" => ["destroy", "build"]
     
     desc "Call Clone and Config tasks for #{container_name}"
-    task "build" => ["clone", "config"]
+    task "build" => ["clone", "config","shutdown","start"]
   end
 end
 
