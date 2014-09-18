7ce6b9cda2a0c5a93bd791674d615d6c5bc11e02
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-09-18 14:35:55 +0900

    [Creator] Fix directory name.

--- creator/import_zabbixdb20.sh
+++ creator/import_zabbixdb20.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /usr/share/doc/zabbix-server-mysql-2.0.11/create/
+cd /usr/share/doc/zabbix-server-mysql-2.0.12/create/
 mysql -uroot zabbix < schema.sql
 mysql -uroot zabbix < images.sql
 mysql -uroot zabbix < data.sql
