f0fdae8fe6c2eaf01388d2beb02b2f07ebdbaf5a
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-09-18 10:29:11 +0900

    [Creator] Add a new function: print_success_message()

--- creator.py
+++ creator.py
@@ -3,6 +3,10 @@ import lxc
 import os
 import sys
 
+def print_success_message(name):
+    print("Create Container: %s" % name)
+
+
 if not os.geteuid() == 0:
     print("You need root permission to use this script.")
     sys.exit(1)
