ee0d81432f871ea97adf3e5cfbbff20a237a038b
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-09-18 11:19:32 +0900

    [Creator] Add process to create base distribution container.

--- creator.py
+++ creator.py
@@ -10,3 +10,18 @@ def print_success_message(name):
 if not os.geteuid() == 0:
     print("You need root permission to use this script.")
     sys.exit(1)
+
+
+base_name = "env_base"
+base = lxc.Container(base_name)
+if not base.defined:
+    base.create("centos")
+    print_success_message("Create Container: %s" % base_name)
+
+    base.start()
+    base.get_ips(timeout=30)
+    base.attach_wait(lxc.attach_run_command,
+                     ["yum", "upgrade", "-y"])
+
+    if not base.shutdown(30):
+        base.stop()
