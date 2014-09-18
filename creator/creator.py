b5c5a296b76cec9d58e97dc985da0e877109492d
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-09-18 15:05:31 +0900

    [Creator] Create a container of zabbix server ver 2.0.

--- creator/creator.py
+++ creator/creator.py
@@ -77,3 +77,55 @@ if not zabbix_server22.defined:
 
     if not zabbix_server22.shutdown(30):
         zabbix_server22.stop()
+
+
+zabbix_server20_name = "env_zabbix_server20"
+zabbix_server20 = lxc.Container(zabbix_server20_name)
+if not zabbix_server20.defined:
+    zabbix_server20 = base.clone(zabbix_server20_name, bdevtype="aufs",
+                                 flags=lxc.LXC_CLONE_SNAPSHOT)
+    print_success_message(zabbix_server20_name)
+
+    rpm_url = "http://repo.zabbix.com/zabbix/2.0/rhel/6/x86_64/zabbix-release-2.0-1.el6.noarch.rpm"
+    script_url = "https://raw.githubusercontent.com/project-hatohol/test-environment-manager/creator/creator/script/import_zabbixdb20.sh"
+    script_name = "import_zabbixdb20.sh"
+    zabbix_server20.start()
+    zabbix_server20.get_ips(timeout=30)
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["rpm", "-ivh", rpm_url])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["yum", "install", "-y",
+                                 "mysql-server", "httpd",
+                                 "zabbix-server-mysql",
+                                 "zabbix-web-mysql"])
+
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["service", "mysqld", "start"])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["chkconfig", "mysqld", "on"])
+
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["mysql", "-uroot", "-e",
+                                 "create database zabbix character set utf8 collate utf8_bin;"])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["mysql", "-uroot", "-e",
+                                 "grant all privileges on zabbix.* to zabbix@localhost identified by 'zabbix';"])
+
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["curl", "-O", script_url])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["chmod", "+x", script_name])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["./" + script_name])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["rm", script_name])
+
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["service", "httpd", "start"])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["chkconfig", "httpd", "on"])
+    zabbix_server20.attach_wait(lxc.attach_run_command,
+                                ["chkconfig", "zabbix-server", "on"])
+
+    if not zabbix_server20.shutdown(30):
+        zabbix_server20.stop()
