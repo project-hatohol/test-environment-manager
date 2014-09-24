9add71b8ef50749be480b609711706e9b7065c40
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-09-24 15:11:25 +0900

    [Checker] Add skelton script to check that the containers are created successfully.

---
+++ creator/checker.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+import lxc
+import os
+import sys
+
+
+if __name__ == '__main__':
+    if not os.geteuid() == 0:
+        print("You need root permission to use this script.")
+        sys.exit(1)
