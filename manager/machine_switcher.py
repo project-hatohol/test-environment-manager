1fee5e0b669c955c4dbc922eeaeba122e95849b3
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-09-25 09:18:39 +0900

    Simplified main statement and Create function that change group statement and machine statement

--- manager/machine_switcher.py
+++ manager/machine_switcher.py
@@ -18,12 +18,29 @@ def change_state(machine_id):
              print(container_name[machine_id] + " | " + "Stop "
                    + str(container_obj[machine_id].stop()))
 
+def change_group_state(group_list):
+    group_dict = create_group_dict()
+    for group in group_list:
+        change_machine_state(group_dict[group])
+
+def change_machine_state(machine_list):
+    for machine_id in machine_list:
+        change_state(machine_id)
+
+def adjust_machine_id(arg_list):
+    machine_no = 0
+    while machine_no < len(arg_list):
+        arg_list[machine_no] -= 1
+        machine_no += 1    
+
+    return arg_list
+
 def separate_arg():
     lists = []
     for select in sys.argv[2:]:
         if "-" in select:
             select = select.split("-")
-            for num in range(int(seelct[0]),int(select[1]) + 1):
+            for num in range(int(select[0]),int(select[1]) + 1):
                 lists.append(num)
         else:
             lists.append(int(select))
@@ -35,7 +52,7 @@ def create_group_dict():
     for machine_id in range(0,len(container_name)):
         group_path = open("/var/lib/lxc/" + container_name[machine_id] + "/group")
         group_lines = group_path.readlines()
-        ld.close()
+        group_path.close()
         group = int(group_lines[0].rstrip())
         
         if group not in dict:
@@ -49,14 +66,10 @@ if __name__ == '__main__':
     arg_list = separate_arg()
     
     if sys.argv[1] == "m":
-        for machine_id in arg_list:
-            change_state(machine_id - 1)
+        change_machine_state(adjust_machine_id(arg_list))
 
     elif sys.argv[1] == "g":
-        group_dict = create_group_dict()
-        for group in arg_list:
-            for machine_id in group_dict[group]:
-                change_state(machine_id)
+        change_group_state(arg_list)
 
     else:
         print("You must input the first argument as 'm' or 'g'.")
