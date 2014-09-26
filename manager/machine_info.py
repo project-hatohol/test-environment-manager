4350f3073cdc6571d1f79cb05dec7457a7439228
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-09-26 13:21:23 +0900

    Rename some function and variable name

--- manager/machine_info.py
+++ manager/machine_info.py
@@ -4,30 +4,31 @@ import lxc
 import sys
 import os.path
 
-container_name = lxc.list_containers()
-container_obj = lxc.list_containers(as_object=True)
+container_list = lxc.list_containers()
+container_obj_list = lxc.list_containers(as_object=True)
 
-def print_frame():
+def print_template():
     print ("-----------------------------------------------------------------")
     print ("%-3s"%"No" + "|" + "%5s"%"Group" + "|" + "%15s"%"Name     |"
            + "%15s"%"HostName   |" + "%15s"%"IP      |" + "%10s"%"State  |")
     print ("-----------------------------------------------------------------")
 
 
-def print_info(dict):
+def print_container_info(dict):
     print("%2s"%str(dict["id"] + 1) + " | " + "%-3s"%dict["group"] 
-          + " | " + "%-12s"%container_name[dict["id"]] + " | " 
+          + " | " + "%-12s"%container_list[dict["id"]] + " | " 
           + "%-12s"%dict["host"] + " | " + "%-12s"%dict["ip"] 
-          + " | " + container_obj[dict["id"]].state + " | ")
+          + " | " + container_obj_list[dict["id"]].state + " | ")
 
 
-def plug_frame(count):
-    if count % 20 == 0 and count != 0:
-        print_frame()
+def plug_template(count):
+    punctuation = 20
+    if count % punctuation == 0 and count != 0:
+        print_template()
 
 
 def get_group_info(info_dict, machine_id):
-    group_file = open("/var/lib/lxc/" + container_name[machine_id] + "/group")
+    group_file = open("/var/lib/lxc/" + container_list[machine_id] + "/group")
     group_lines = group_file.readlines()
     group_file.close()
 
@@ -35,35 +36,34 @@ def get_group_info(info_dict, machine_id):
 
 
 def get_config_info(info_dict, machine_id):
-    conf_file = open("/var/lib/lxc/" + container_name[machine_id] + "/config")
+    conf_file = open("/var/lib/lxc/" + container_list[machine_id] + "/config")
     conf_lines = conf_file.readlines()
     conf_file.close()
 
     for line in conf_lines:
         if line.find("lxc.network.ipv4 =") >= 0:
-            (trash,address_and_mask) = line.split("=")
-            (address,mask) = address_and_mask.split("/")
+            (key, address_and_mask) = line.split("=")
+            (address, mask) = address_and_mask.split("/")
             info_dict["ip"] = address
 
         elif line.find("lxc.utsname =") >= 0:
-            (trash,host) = line.split("=")
+            (key, host) = line.split("=")
             info_dict["host"] = host.rstrip()
 
 
 def get_info_dict(machine_id):
     info_dict = {}
-    get_config_info(info_dict,machine_id)
-    get_group_info(info_dict,machine_id)
+    get_config_info(info_dict, machine_id)
+    get_group_info(info_dict, machine_id)
     info_dict["id"] = machine_id
 
     return info_dict
 
 
 if __name__ == '__main__':
-    print_frame()
+    print_template()
 
-    container_list_len = len(container_name)
-    for machine_id in range(container_list_len):
-        plug_frame(machine_id)
-        print_info(get_info_dict(machine_id))
+    for machine_id in range(len(container_list)):
+        plug_template(machine_id)
+        print_container_info(get_info_dict(machine_id))
 
--- manager/machine_switcher.py
+++ manager/machine_switcher.py
@@ -4,12 +4,12 @@ import lxc
 import sys
 import os.path
 
-container_name = lxc.list_containers()
-container_obj = lxc.list_containers(as_object=True)
+container_list = lxc.list_containers()
+container_obj_list = lxc.list_containers(as_object=True)
 
-def change_state(machine_id):
-    obj = container_obj[machine_id]
-    name = container_name[machine_id]
+def toggle_state(machine_id):
+    obj = container_obj_list[machine_id]
+    name = container_list[machine_id]
     if obj.state == "STOPPED":
             start_result = str(obj.start())
             print(name + " | " + "Start " + start_result)
@@ -21,18 +21,18 @@ def change_state(machine_id):
              print(name + " | " + "Stop " + str(stop))
 
 
-def change_group_state(group_id_list):
+def toggle_state_for_group(group_id_list):
     group_dict = create_group_dict()
     for group_id in group_id_list:
-        change_machine_state(group_dict[group_id])
+        toglle_each_machine_state(group_dict[group_id])
 
 
-def change_machine_state(machine_id_list):
+def toggle_each_machine_state(machine_id_list):
     for machine_id in machine_id_list:
-        change_state(machine_id)
+        toggle_state(machine_id)
 
 
-def convert_machine_num_to_id(machine_num_list):
+def convert_machine_nums_to_ids(machine_num_list):
     machine_id_list = [int(machine_num) - 1 for machine_num in machine_num_list]    
 
     return machine_id__list
@@ -40,11 +40,10 @@ def convert_machine_num_to_id(machine_num_list):
 
 def separate_id_list():
     lists = []
-    for select_id in sys.argv[2:]:
+    select_arg = 2
+    for select_id in sys.argv[select_arg:]:
         if "-" in select_id:
-            max_min = select_id.split("-")
-            max = max_min[1]
-            min = max_min[0]
+            (min, max) = select_id.split("-")
             for id in range(min, max+1):
                 lists.append(id)
         else:
@@ -54,9 +53,9 @@ def separate_id_list():
 
 def create_group_dict():
     dict = {}
-    container_list_len = len(container_name)
+    container_list_len = len(container_list)
     for machine_id in range(container_list_len):
-        group_file = open("/var/lib/lxc/" + container_name[machine_id] + "/group")
+        group_file = open("/var/lib/lxc/" + container_list[machine_id] + "/group")
         group_lines = group_file.readlines()
         group_file.close()
         group_id = int(group_lines[0].rstrip())
@@ -73,11 +72,11 @@ if __name__ == '__main__':
     if sys.argv[1] == "m":   
         machine_num_list = separate_id_list()
         machine_id_list = convert_machine_num_to_id(machine_num_list)
-        change_machine_state(machine_id_list)
+        toggle_each_machine_state(machine_id_list)
 
     elif sys.argv[1] == "g":
         group_id_list = separate_id_list()
-        change_group_state(group_id_list)
+        toggle_state_for_group(group_id_list)
 
     else:
         print("You must input the first argument as 'm' or 'g'.")
