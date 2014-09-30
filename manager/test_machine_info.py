8c37c16bc16bf4c9a3160719c555d88c930c7201
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-09-30 17:39:05 +0900

    Create functions that test the print_header function

--- manager/test_machine_info.py
+++ manager/test_machine_info.py
@@ -2,12 +2,35 @@
 
 import machine_info
 import unittest
+import sys
+
+def get_output(file_name, func_name, **kwargs):
+    sys.stdout = open(file_name, "w")
+    func_name(**kwargs)
+    sys.stdout.close()
+    sys.stdout = sys.__stdout__
+ 
+
+def read_file(file_name):
+    file = open(file_name)
+    lines = file.readlines()
+    file.close()
+
+    return lines
+
 
 test_dict = {"id":0, "host":"machine1_1", "group":"1", "ip":"10.0.3.11"}
 
 class TestMachineInfo(unittest.TestCase):
     def test_get_info_dict(self):
         self.assertEqual(machine_info.get_info_dict(0), test_dict)
+
+
+    def test_print_header(self):
+        get_output("header_output", machine_info.print_header)
+        lines = read_file("header_output")
+        self.assertTrue("-" in lines[0])
+        self.assertTrue("No" in lines[1])
 
 
 if __name__ == '__main__':
