cc1254525f6187f0f50dd777e3b172343c36ea4c
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-09-30 11:21:40 +0900

    Create a function that test the get_info_dict function

--- manager/test_machine_info.py
+++ manager/test_machine_info.py
@@ -3,6 +3,13 @@
 import machine_info
 import unittest
 
+test_dict = {"id":0, "host":"machine1_1", "group":"1", "ip":"10.0.3.11"}
+
+class TestMachineInfo(unittest.TestCase):
+    def test_get_info_dict(self):
+        self.assertEqual(machine_info.get_info_dict(0), test_dict)
+
+
 if __name__ == '__main__':
     unittest.main()
 
