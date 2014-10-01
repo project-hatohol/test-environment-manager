f1a797d30c52132a5daa0c5936edf88ce4e6df5a
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-01 15:51:09 +0900

    Create function that test the toggle_state function

--- manager/test_machine_switcher.py
+++ manager/test_machine_switcher.py
@@ -3,6 +3,16 @@
 import machine_switcher
 import unittest
 import sys
+import lxc 
+
+container_objs = lxc.list_containers(as_object = True)
+
+class TestMachineInfo(unittest.TestCase):
+    def test_toggle_state(self):
+        before_state = container_objs[0].state
+        machine_switcher.toggle_state(0)
+        self.assertFalse(before_state == container_objs[0].state)
+
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
