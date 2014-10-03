0416ba9d87c45d22ddca4c1293d0c7919859e973
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-03 10:23:02 +0900

    Fix typo: pass -> path

--- manager/machine_info.py
+++ manager/machine_info.py
@@ -25,30 +25,30 @@ def insert_header(exe_count):
         print_header()
 
 
-def read_file(container_pass, file_name):
-    file = open(container_pass + file_name)
+def read_file(container_path, file_name):
+    file = open(container_path + file_name)
     lines = file.readlines()
     file.close()
 
     return lines
 
 
-def get_container_pass(container_name):
-    container_pass = "/var/lib/lxc/" + container_name + "/"
+def get_container_path(container_name):
+    container_path = "/var/lib/lxc/" + container_name + "/"
 
-    return container_pass
+    return container_path
 
 
 def get_id_info(info_dict, machine_id):
     info_dict["id"] = machine_id
 
 
-def get_group_info(info_dict, container_pass):
-    info_dict["group"] = read_file(container_pass, "group")[0].rstrip()
+def get_group_info(info_dict, container_path):
+    info_dict["group"] = read_file(container_path, "group")[0].rstrip()
 
 
-def get_config_info(info_dict, container_pass):
-    conf_lines = read_file(container_pass, "config")
+def get_config_info(info_dict, container_path):
+    conf_lines = read_file(container_path, "config")
     for line in conf_lines:
         if line.find("lxc.network.ipv4") >= 0 and line.find("/") >= 0:
             (key, address_and_mask) = line.split("=")
@@ -60,11 +60,11 @@ def get_config_info(info_dict, container_pass):
             info_dict["host"] = host.strip()
 
 
-def get_info_dict(machine_id, container_pass):
+def get_info_dict(machine_id, container_path):
     info_dict = {}
     get_id_info(info_dict, machine_id)
-    get_config_info(info_dict, container_pass)
-    get_group_info(info_dict, container_pass)
+    get_config_info(info_dict, container_path)
+    get_group_info(info_dict, container_path)
 
     return info_dict
 
@@ -74,8 +74,8 @@ if __name__ == '__main__':
     container_list = lxc.list_containers()
 
     for exe_count in range(len(container_list)):
-        container_pass = get_container_pass(container_list[exe_count])
+        container_path = get_container_path(container_list[exe_count])
         insert_header(exe_count)
-        print_container_info(get_info_dict(exe_count, container_pass),
+        print_container_info(get_info_dict(exe_count, container_path),
                              container_list, container_obj_list)
 
