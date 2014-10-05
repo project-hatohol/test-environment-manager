3647a4be04baa0d99c91e3aae25074c5f6263f0e
Noriki Nakamura <bynn_chapu@tencho-lab.net>
2014-10-05 19:07:00 +0900

    [Script] Use '*' instead of defined value.

--- creator/script/import_zabbixdb22.sh
+++ creator/script/import_zabbixdb22.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd /usr/share/doc/zabbix-server-mysql-2.2.6/create/
+cd /usr/share/doc/zabbix-server-mysql-2.2.*/create/
 mysql -uroot zabbix < schema.sql
 mysql -uroot zabbix < images.sql
 mysql -uroot zabbix < data.sql
