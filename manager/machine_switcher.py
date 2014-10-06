e44b935273ae2d92b43b8b0e79e196ac294613d6
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-06 14:32:23 +0900

    Change global variables to local variables

--- manager/machine_switcher.py
+++ manager/machine_switcher.py
@@ -5,12 +5,7 @@ import sys
 import os.path
 import machine_info
 
-container_list = lxc.list_containers()
-container_obj_list = lxc.list_containers(as_object=True)
-
-def toggle_state(machine_id):
-    obj = container_obj_list[machine_id]
-    name = container_list[machine_id]
+def toggle_state(obj, name):
     if obj.state == "STOPPED":
         change_state_to_start(obj, name)
     else:
@@ -30,15 +25,15 @@ def change_state_to_stop(obj, name):
         print(name + " | " + "Stop " + str(succeed_stop))
 
 
-def toggle_state_for_group(group_id_list, container_dir_path):
-    group_dict = create_group_dict(container_dir_path)
+def toggle_state_for_group(group_id_list, container_dir_path, container_list, container_obj_list):
+    group_dict = create_group_dict(container_dir_path, container_list)
     for group_id in group_id_list:
-        toggle_state_for_each_machine(group_dict[group_id])
+        toggle_state_for_each_machine(group_dict[group_id],container_obj_list, container_list)
 
 
-def toggle_state_for_each_machine(machine_id_list):
+def toggle_state_for_each_machine(machine_id_list, container_obj_list, container_list):
     for machine_id in machine_id_list:
-        toggle_state(machine_id)
+        toggle_state(container_obj_list[machine_id], container_list[machine_id])
 
 
 def convert_machine_nums_to_ids(machine_num_list):
@@ -58,7 +53,7 @@ def enum_id_list(input_argument):
     return lists
 
 
-def create_group_dict(container_dir_path):
+def create_group_dict(container_dir_path, container_list):
     dict = {}
     for machine_id in range(len(container_list)):
         container_path = container_dir_path + container_list[machine_id] + "/"
@@ -79,16 +74,21 @@ def get_container_dir_path():
 
 
 if __name__ == '__main__':
+    container_list = lxc.list_containers()
+    container_obj_list = lxc.list_containers(as_object=True)
     input_argument = sys.argv[2:]
+
     if sys.argv[1] == "m":   
         machine_num_list = enum_id_list(input_argument)
         machine_id_list = convert_machine_nums_to_ids(machine_num_list)
-        toggle_state_for_each_machine(machine_id_list)
+        toggle_state_for_each_machine(machine_id_list, container_obj_list)
 
     elif sys.argv[1] == "g":
         container_dir_path = get_container_dir_path()
         group_id_list = enum_id_list(input_argument)
-        toggle_state_for_group(group_id_list, container_dir_path)
+        toggle_state_for_group(enum_id_list(input_argument),
+                               get_container_dir_path(),
+                               container_list, container_obj_list)
 
     else:
         print("You must input the first argument as 'm' or 'g'.")
