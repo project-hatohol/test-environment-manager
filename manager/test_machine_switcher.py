54b6059b293363486d9812706d04a25bd111bd2d
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-06 18:13:54 +0900

    Create a new function: _compare_container_state

--- manager/test_machine_switcher.py
+++ manager/test_machine_switcher.py
@@ -11,6 +11,13 @@ class TestMachineInfo(unittest.TestCase):
     container_obj_list = test_stub.test_obj_list()
     test_container_dir_path = os.path.dirname(os.path.abspath(__file__)) + "/"
 
+    def _compare_container_state(self, container_id):
+        before_state = self.container_obj_list[container_id].state
+        machine_switcher.toggle_state(self.container_obj_list[container_id],
+                                      self.container_list[container_id])
+        self.assertFalse(before_state == self.container_obj_list[container_id].state)
+
+
 if __name__ == '__main__':
     unittest.main()
 
