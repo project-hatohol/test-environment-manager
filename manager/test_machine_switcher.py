eccd7c819cad21e1ca70d5ea36824e74043b5003
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-07 09:48:00 +0900

    Change container_obj_list to private variables

--- manager/test_machine_switcher.py
+++ manager/test_machine_switcher.py
@@ -8,21 +8,21 @@ import os
 
 class TestMachineInfo(unittest.TestCase):
     _container_list = test_stub.test__container_list()
-    container_obj_list = test_stub.test_obj_list()
+    _container_obj_list = test_stub.test_obj_list()
     test_container_dir_path = os.path.dirname(os.path.abspath(__file__)) + "/"
     test_id = 0
 
     def _assert_container_state(self, container_id):
-        previous_state = self.container_obj_list[container_id].state
-        machine_switcher.toggle_state(self.container_obj_list[container_id],
+        previous_state = self._container_obj_list[container_id].state
+        machine_switcher.toggle_state(self._container_obj_list[container_id],
                                       self._container_list[container_id])
-        self.assertFalse(previous_state == self.container_obj_list[container_id].state)
+        self.assertFalse(previous_state == self._container_obj_list[container_id].state)
 
 
     def _get_containers_state(self, container_ids):
         states = []
         for container_id in container_ids:
-            states.append(self.container_obj_list[container_id].state)
+            states.append(self._container_obj_list[container_id].state)
 
         return states
 
@@ -32,22 +32,22 @@ class TestMachineInfo(unittest.TestCase):
 
 
     def test_change_state_to_start(self):
-        machine_switcher.change_state_to_start(self.container_obj_list[self.test_id],
+        machine_switcher.change_state_to_start(self._container_obj_list[self.test_id],
                                                self._container_list[self.test_id])
-        self.assertEquals("RUNNING", self.container_obj_list[self.test_id].state)
+        self.assertEquals("RUNNING", self._container_obj_list[self.test_id].state)
 
 
     def test_change_state_to_stop(self):
-        machine_switcher.change_state_to_stop(self.container_obj_list[self.test_id],
+        machine_switcher.change_state_to_stop(self._container_obj_list[self.test_id],
                                                self._container_list[self.test_id])
-        self.assertEquals("STOPPED", self.container_obj_list[self.test_id].state)
+        self.assertEquals("STOPPED", self._container_obj_list[self.test_id].state)
 
 
     def test_toggle_state_for_group(self):
         test_ids = [0, 1, 2, 3, 4, 5]
         previous_states = self._get_containers_state(test_ids)
         machine_switcher.toggle_state_for_group([1, 2], self.test_container_dir_path,
-                                                self._container_list, self.container_obj_list)
+                                                self._container_list, self._container_obj_list)
         current_states = self._get_containers_state(test_ids)
 
         for num in range(len(test_ids)):
@@ -57,7 +57,7 @@ class TestMachineInfo(unittest.TestCase):
     def test_toggle_state_for_each_machine(self):
         test_ids = [0, 1, 3]
         previous_states = self._get_containers_state(test_ids)
-        machine_switcher.toggle_state_for_each_machine(test_ids, self.container_obj_list,
+        machine_switcher.toggle_state_for_each_machine(test_ids, self._container_obj_list,
                                                        self._container_list)
         current_states = self._get_containers_state(test_ids)
 
