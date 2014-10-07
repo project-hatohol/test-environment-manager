a1b2c019c92072efe67c6dfc6285456f87b20bd8
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-07 09:13:17 +0900

    Delete extra codes

--- manager/test_stub.py
+++ manager/test_stub.py
@@ -19,36 +19,14 @@ def test_obj_list():
     return test_objs
 
 
-def read_file(test_state_path):
-    file = open(test_state_path)
-    lines = file.readlines()
-    file.close()
-
-    return lines
-
-
-def write_state(test_state_path, test_container_state):
-    file = open(test_state_path, "w")
-    file.write(test_container_state)
-    file.close()
-
-
-def get_test_container_state(test_container_name, test_state_path):
-    container_state = read_file(test_state_path)[0].strip("\n")
-        
-    return container_state
-
-
 class TestObjList():
     def __init__(self, test_container_name):
         self.test_state_path = os.path.dirname(os.path.abspath(__file__)) + "/" + test_container_name + "/state"
-        self.state = get_test_container_state(test_container_name, self.test_state_path)
-
+        self.state = "STOPPED"
 
     def start(self):
         if self.state == "STOPPED":
             self.state = "RUNNING"
-            write_state(self.test_state_path, self.state)
 
         return True
 
@@ -56,7 +34,6 @@ class TestObjList():
     def stop(self): 
         if self.state == "RUNNING":
             self.state = "STOPPED"
-            write_state(self.test_state_path, self.state)
 
         return True
 
@@ -64,7 +41,6 @@ class TestObjList():
     def shutdown(self):
         if self.state == "RUNNING":
             self.state = "STOPPED"
-            write_state(self.test_state_path, self.state)
 
         return True
 
