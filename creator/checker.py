12f1a95c8150a74211dc759716a0607e4659e4f8
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-07 12:55:24 +0900

    [Checker] Use seaquence for arguments of print_result_of_check_item() function.

--- creator/checker.py
+++ creator/checker.py
@@ -36,72 +36,30 @@ def print_result_of_check_item(container_name, list_of_path):
     shutdown_container(container)
 
 
-def print_installation_result_of_zabbix_server(container_name):
-    LIST_OF_PATH = ["/usr/sbin/zabbix_server", "/usr/sbin/zabbix_agentd"]
-
-    print_result_of_check_item(container_name, LIST_OF_PATH)
-
-
-def print_installation_result_of_zabbix_agent(container_name):
-    LIST_OF_PATH = ["/usr/sbin/zabbix_agentd"]
-
-    print_result_of_check_item(container_name, LIST_OF_PATH)
-
-
-def print_installation_result_of_nagios_server3():
-    LIST_OF_PATH = ["/usr/sbin/nagios", "/usr/sbin/ndo2db"]
-    CONTAINER_NAME = "env_nagios_server3"
-
-    print_result_of_check_item(CONTAINER_NAME, LIST_OF_PATH)
-
-
-def print_installation_result_of_nagios_server4():
-    LIST_OF_PATH = ["/usr/local/nagios/bin/nagios",
-                  "/usr/local/nagios/bin/ndo2db"]
-    CONTAINER_NAME = "env_nagios_server4"
-
-    print_result_of_check_item(CONTAINER_NAME, LIST_OF_PATH)
-
-
-def print_installation_result_of_nagios_nrpe():
-    LIST_OF_PATH = ["/etc/nagios/nrpe.cfg", "/usr/lib64/nagios/plugins/"]
-    CONTAINER_NAME = "env_nagios_nrpe"
-
-    print_result_of_check_item(CONTAINER_NAME, LIST_OF_PATH)
-
-
-def print_installation_result_of_hatohol():
-    LIST_OF_PATH = ["/usr/sbin/hatohol"]
-    CONTAINER_NAME = "env_hatohol_rpm"
-
-    print_result_of_check_item(CONTAINER_NAME, LIST_OF_PATH)
-
-
-def print_installation_result_of_redmine():
-    LIST_OF_PATH = ["/var/lib/redmine", "/usr/local/bin/ruby"]
-    CONTAINER_NAME = "env_redmine"
-
-    print_result_of_check_item(CONTAINER_NAME, LIST_OF_PATH)
-
-
-def print_installation_result_of_fluentd():
-    LIST_OF_PATH = ["/usr/sbin/td-agent"]
-    CONTAINER_NAME = "env_fluentd"
-
-    print_result_of_check_item(CONTAINER_NAME, LIST_OF_PATH)
-
-
 def print_installation_result():
-    print_installation_result_of_hatohol()
-    print_installation_result_of_zabbix_server("env_zabbix_server22")
-    print_installation_result_of_zabbix_server("env_zabbix_server20")
-    print_installation_result_of_zabbix_agent("env_zabbix_agent22")
-    print_installation_result_of_zabbix_agent("env_zabbix_agent20")
-    print_installation_result_of_nagios_server3()
-    print_installation_result_of_nagios_server4()
-    print_installation_result_of_nagios_nrpe()
-    print_installation_result_of_redmine()
-    print_installation_result_of_fluentd()
+    ARGS = [["env_zabbix_server22",
+             ["/usr/sbin/zabbix_server", "/usr/sbin/zabbix_agentd"]],
+            ["env_zabbix_server20",
+             ["/usr/sbin/zabbix_server", "/usr/sbin/zabbix_agentd"]],
+            ["env_zabbix_agent22",
+             ["/usr/sbin/zabbix_agentd"]],
+            ["env_zabbix_agent20",
+             ["/usr/sbin/zabbix_agentd"]],
+            ["env_nagios_server3",
+             ["/usr/sbin/nagios", "/usr/sbin/ndo2db"]],
+            ["env_nagios_server4",
+             ["/usr/local/nagios/bin/nagios", "/usr/local/nagios/bin/ndo2db"]],
+            ["env_nagios_nrpe",
+             ["/etc/nagios/nrpe.cfg", "/usr/lib64/nagios/plugins/"]],
+            ["env_hatohol_rpm",
+             ["/usr/sbin/hatohol"]],
+            ["env_redmine",
+             ["/var/lib/redmine", "/usr/local/bin/ruby"]],
+            ["env_fluentd",
+             ["/usr/sbin/td-agent"]]]
+
+    for (container_name, list_of_path_to_check) in ARGS:
+        print_result_of_check_item(container_name, list_of_path_to_check)
 
 
 if __name__ == '__main__':
