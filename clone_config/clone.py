4defe44e9cefa4293818cbed2bce69eac26b7154
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-08 10:47:34 +0900

    [Clone] Add a new function: get_dictionary_from_yaml_file()

--- clone_config/clone.py
+++ clone_config/clone.py
@@ -4,6 +4,11 @@ import sys
 import yaml
 import lxc
 
+def get_dictionary_from_yaml_file(file_path):
+    yaml_file = open(file_path).read()
+    return yaml.load(yaml_file)
+
+
 if __name__ == '__main__':
     if not os.geteuid() == 0:
         print("You need root permission to use this script.")
