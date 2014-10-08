42479d1d6536cd04090493e9446bfa1dcf12619d
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-08 16:05:31 +0900

    [Common][Utils] Move the functions in clone_config/ directory to utils.py file in common/ directory.

--- common/utils.py
+++ common/utils.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
+import sys
+import os
 import definevalue
+
+def finish_if_argument_is_not_given(number_of_argument):
+    if (number_of_argument == 1):
+        print("Error: argument is not given")
+        sys.exit(1)
+
+
+def finish_if_user_run_as_general_user():
+    if not os.geteuid() == 0:
+        print("Error: You need root permission to use this script.")
+        sys.exit(1)
+
 
 def print_success_message(name):
     print("Create Container: %s" % name)
