7157fe9318eb7d4af1c8c8a08b36e1908799f587
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-08 17:37:47 +0900

    [Common][Definevalue] Add a constant value: EXIT_FAILURE

--- common/utils.py
+++ common/utils.py
@@ -6,13 +6,13 @@ import definevalue
 def finish_if_argument_is_not_given(number_of_argument):
     if (number_of_argument == 1):
         print("Error: argument is not given")
-        sys.exit(1)
+        sys.exit(definevalue.EXIT_FAILURE)
 
 
 def finish_if_user_run_as_general_user():
     if not os.geteuid() == 0:
         print("Error: You need root permission to use this script.")
-        sys.exit(1)
+        sys.exit(definevalue.EXIT_FAILURE)
 
 
 def print_success_message(name):
