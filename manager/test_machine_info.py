7552f60f975057bee28ee19a955d4b7a5a9cb8df
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-08 10:04:24 +0900

    Fix a English comment

--- manager/test_machine_info.py
+++ manager/test_machine_info.py
@@ -10,7 +10,7 @@ def _get_output(func_name, **kwargs):
     (read_fd, write_fd) = os.pipe()
 
     sys.stdout = os.fdopen(write_fd, "w")
-    func_name(**kwargs) #Print statement is blocked when it print many output 
+    func_name(**kwargs) # A print function is blocked when it output to many.
     sys.stdout.close()
     sys.stdout = sys.__stdout__
 
