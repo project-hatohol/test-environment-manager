aadd2c6abaecabea945ebba6bd77d0bcca02cc57
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-08 12:09:49 +0900

    [Clone] Add a new function: get_container_name_and_base_container()

--- clone_config/clone.py
+++ clone_config/clone.py
@@ -4,9 +4,25 @@ import sys
 import yaml
 import lxc
 
+NAME_BASE_CONTAINER = "base_container"
+
 def get_dictionary_from_yaml_file(file_path):
     yaml_file = open(file_path).read()
     return yaml.load(yaml_file)
+
+
+def get_container_name_and_base_container(dictionary_name):
+    list_of_container_name = dictionary_name.keys()
+    return_list = []
+    for container_name in list_of_container_name:
+        if not NAME_BASE_CONTAINER in dictionary_name[container_name]:
+            continue
+        else:
+            return_list.append([container_name,
+                                dictionary_name[container_name]
+                                [NAME_BASE_CONTAINER]])
+
+    return return_list
 
 
 if __name__ == '__main__':
