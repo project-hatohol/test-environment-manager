864daa31d47b97d8179c8963b127bf09874fa008
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-09 10:12:30 +0900

    [Common][Definevalue] Add constant value of dictionary type to assign function to set up for each softwares.
    I add a skelton of these functions.

--- clone_config/setup.py
+++ clone_config/setup.py
@@ -7,6 +7,34 @@ sys.path.append("../common")
 import definevalue
 from utils import *
 
+def run_setup_zabbix_server():
+    print("Not implemented yet: run_setup_zabbix_server")
+
+
+def run_setup_zabbix_agent():
+    print("Not implemented yet: run_setup_zabbix_agent")
+
+
+def run_setup_nagios_server3():
+    print("Not implemented yet: run_setup_nagios_server3")
+
+
+def run_setup_nagios_server4():
+    print("Not implemented yet: run_setup_nagios_server4")
+
+
+def run_setup_nagios_nrpe():
+    print("Not implemented yet: run_setup_nagios_nrpe")
+
+
+def run_setup_redmine():
+    print("Not implemented yet: run_setup_redmine")
+
+
+def run_setup_fluentd():
+    print("Not implemented yet: run_setup_fluentd")
+
+
 if __name__ == '__main__':
     argvs = sys.argv
     exit_if_user_run_this_as_general_user()
--- common/definevalue.py
+++ common/definevalue.py
@@ -6,5 +6,13 @@ CONTAINER_NAMES = ["env_base", "env_zabbix_server22", "env_zabbix_server20",
                   "env_nagios_nrpe", "env_hatohol_build", "env_hatohol_rpm",
                   "env_fluentd", "env_redmine"]
 
+SETUP_FUNCTIONS = {"zabbix-server": run_setup_zabbix_server,
+                   "zabbix-agent": run_setup_zabbix_agent,
+                   "nagios3": run_setup_nagios_server3,
+                   "nagios4": run_setup_nagios_server4,
+                   "nrpe": run_setup_nagios_nrpe,
+                   "redmine": run_setup_redmine,
+                   "fluentd": run_setup_fluentd}
+
 KEY_OF_BASE_CONTAINER = "base_container"
 TIMEOUT_VALUE = 30
