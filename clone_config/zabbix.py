f1b419b49d65aa5174a23dc9649955ff24e51c6b
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-14 20:22:56 +0900

    [Zabbix] Add a new function: send_data_and_get_response()

--- clone_config/zabbix.py
+++ clone_config/zabbix.py
@@ -6,3 +6,10 @@ import requests
 import json
 sys.path.append("../common")
 import definevalue
+
+def send_data_and_get_response(send_content):
+    send_data = json.dumps(send_content)
+    response = requests.post(definevalue.ZABBIX_SERVER_ADDRESS,
+                             data=send_data,
+                             headers=definevalue.ZABBIX_API_HEADER)
+    return response.json()
