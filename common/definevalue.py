f138f529ffc85ff87f53c913b9e23d7babbda4b7
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-14 16:54:11 +0900

    [Definevalue] Add two constant values.
    - ZABBIX_SERVER_ADDRESS
    - ZABBIX_API_HEADER

--- common/definevalue.py
+++ common/definevalue.py
@@ -8,3 +8,6 @@ CONTAINER_NAMES = ["env_base", "env_zabbix_server22", "env_zabbix_server20",
 
 KEY_OF_BASE_CONTAINER = "base_container"
 TIMEOUT_VALUE = 30
+
+ZABBIX_SERVER_ADDRESS = "http://127.0.0.1/zabbix/api_jsonrpc.php"
+ZABBIX_API_HEADER = {"content-type": "application/json"}
