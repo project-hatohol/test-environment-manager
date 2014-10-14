3a9c22717c6f5c13e600d444f230d810510008e4
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-14 20:23:37 +0900

    [Zabbix] Add a new function: get_authtoken_of_zabbix_server()

--- clone_config/zabbix.py
+++ clone_config/zabbix.py
@@ -13,3 +13,15 @@ def send_data_and_get_response(send_content):
                              data=send_data,
                              headers=definevalue.ZABBIX_API_HEADER)
     return response.json()
+
+
+def get_authtoken_of_zabbix_server():
+    SEND_CONTENT = {"method": "user.login",
+                    "id": 1,
+                    "params": {
+                        "password": "zabbix",
+                        "user":"Admin"},
+                    "jsonrpc": "2.0"}
+    response_json = send_data_and_get_response(SEND_CONTENT)
+
+    return response_json["result"]
