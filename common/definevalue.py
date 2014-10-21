b272ed4b91a41ff88d19c4807122f7a2e06377f8
Masayuki Nakagawa <masayuki.nakagawa@miraclelinux.com>
2014-10-21 16:39:32 +0900

    Add values in definevalue.py

--- common/definevalue.py
+++ common/definevalue.py
@@ -11,3 +11,33 @@ TIMEOUT_VALUE = 30
 
 ZABBIX_SERVER_ADDRESS = "http://127.0.0.1/zabbix/api_jsonrpc.php"
 ZABBIX_API_HEADER = {"content-type": "application/json"}
+
+ZABBIX_CONF_SERVER_PATH = "/etc/zabbix/zabbix_server.conf"
+ZABBIX_CONF_HTTPD_PATH = "/etc/httpd/conf.d/zabbix.conf"
+ZABBIX_CONF_PHP_PATH = "/etc/zabbix/web/zabbix.conf.php"
+
+ZABBIX_CONF_FILE_PATH = "/etc/zabbix/zabbix_agentd.conf"
+
+NAGIOS3_LIST_OF_PATH = {"CONFIG": "/etc/nagios/nagios.cfg",
+                        "COMMANDS": "/etc/nagios/objects/commands.cfg",
+                        "SERVER_DIR": "/etc/nagios/servers/",
+                        "PASSWORD": "/etc/nagios/passwd",
+                        "CGI": "/etc/nagios/cgi.cfg",
+                        "NDO2DB": "/etc/nagios/ndo2db.cfg"}
+
+NAGIOS4_LIST_OF_PATH = {"CONFIG": "/usr/local/nagios/etc/nagios.cfg",
+                        "COMMANDS": "/usr/local/nagios/etc/objects/commands.cfg",
+                        "SERVER_DIR": "/usr/local/nagios/etc/servers/",
+                        "PASSWORD": "/usr/local/nagios/etc/htpasswd.users",
+                        "CGI": "/usr/local/nagios/etc/cgi.cfg",
+                        "NDO2DB": "/usr/local/nagios/etc/ndo2db.cfg"}
+
+NRPE_FILE_PATH = "/etc/nagios/nrpe.cfg"
+
+REDMINE_LIST_OF_PATH = ["/var/lib/redmine/config/database.yml",
+                        "/var/lib/redmine/config/configuration.yml",
+                        "/var/lib/redmine/my_setting",
+                        "/var/lib/redmine/setting_command.sh"]
+
+TD_AGENT_FILE_PATH = "/etc/td-agent/td-agent.conf"
+
