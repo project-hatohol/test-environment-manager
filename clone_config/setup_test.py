b6c498324bc9385a18800e7c06703eba721749b2
Noriki Nakamura <noriki.nakamura@miraclelinux.com>
2014-10-24 15:37:44 +0900

    [SetupTest] Add a new line to apply pep8

--- clone_config/setup_test.py
+++ clone_config/setup_test.py
@@ -14,6 +14,7 @@ import utils
 import definevalue
 import zabbix
 
+
 def add_according_key(setting_dict, container_name, setup_element_key):
     setup_functions = ["zabbix-server", "zabbix-agent", "nagios3",
                        "nagios4", "nrpe", "redmine", "fluentd"]
